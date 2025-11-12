21ca7701764fde9ff0ba0832f2ca7107da1c14a4
danixlc00 <bigcow315@gmail.com>
2025-11-12 20:21:37 +0800

    update fonts for dashboard

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ import dash_bootstrap_components as dbc
 from thematic_dash import get_thematic_layout, register_thematic_callbacks
 from security_dashboard import get_security_layout, register_security_callbacks
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY], suppress_callback_exceptions=True)
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY, dbc.icons.BOOTSTRAP], suppress_callback_exceptions=True)
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     dbc.Container([
-        dcc.Tabs(id="tabs", value='tab-thematic', children=[
-            dcc.Tab(label='Thematic Dashboard', value='tab-thematic'),
-            dcc.Tab(label='Security Dashboard', value='tab-security')
-        ]),
+        dcc.Tabs(
+            id="tabs", 
+            value='tab-thematic', 
+            children=[
+                dcc.Tab(label='Thematic Dashboard', value='tab-thematic', style={'color': '#000000'}),
+                dcc.Tab(label='Security Dashboard', value='tab-security', style={'color': '#000000'})
+            ]
+        ),
         html.Div(id='tabs-content', style={'marginTop': '1em'})
     ], fluid=True),
 ])
@@ -23,9 +27,8 @@ def render_tab_content(tab):
     elif tab == 'tab-security':
         return get_security_layout()
 
-# Register callbacks from both dashboards
 register_thematic_callbacks(app)
 register_security_callbacks(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8052)
+    app.run(mode='inline', port=8052, debug=False)
